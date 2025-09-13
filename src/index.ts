cf0a526ea4d7f5f2a0eabdfff31f7628b5bfa3b4
keitakn <keita.koga.work@gmail.com>
2025-09-13 13:12:37 +0900

    :art: agent版のルーティングを追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { seleniumWebFormHandler } from "./handlers/seleniumWebForm.js";
+import { seleniumWebFormWithAgentHandler } from "./handlers/seleniumWebFormWithAgent.js";
 
 const app = new Hono();
 
 const selenium = new Hono();
 selenium.post("/webform", seleniumWebFormHandler);
+selenium.post("/webform/agent", seleniumWebFormWithAgentHandler);
 app.route("/selenium", selenium);
 
 app.get("/healthz", (c) => c.text("ok"));
